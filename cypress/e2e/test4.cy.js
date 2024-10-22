0b4d22f5213d02907ce2356fbd0a4a5119ecd50e
robija92 <robertegon.djeri@gmail.com>
2024-10-22 22:33:43 +0200

    028 Navigating browser controls using Cypress

---
+++ cypress/e2e/test4.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('Test Suite', function () {
+
+    it('Checkbox Test Case', function () {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        cy.get('#alertbtn').click()
+        cy.get('#confirmbtn').click()
+        // Window:alert
+        cy.on('window:alert', (str)=>
+        {
+            //Mocha
+            expect(str).to.equal('Hello , share this practice page and share your knowledge')
+        })
+
+        cy.on('window:confirm', (str)=>
+            {
+                //Mocha
+                expect(str).to.equal('Hello , Are you sure you want to confirm?')
+            })
+
+            cy.get('#opentab').invoke('removeAttr', 'target').click()
+
+            cy.url().should('include', 'qaclickacademy')
+
+            // cy.go('back')    
+
+    })
+
+})
---
+++ cypress/e2e/test5.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Test Suite', function () {
+
+    it('Checkbox Test Case', function () {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        
+
+
+    })
+
+})
